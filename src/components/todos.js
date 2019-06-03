b23eb54ca13f2ce9010b5cdb083bc0e1d2cf3c3c
Eddcakes <eddadamson@gmail.com>
2019-06-03 15:13:31 +0100

    added display for no items available

--- src/components/todos.js
+++ src/components/todos.js
@@ -2,6 +2,7 @@ import React, { useState, useMemo } from 'react';
 import TodoItem from './todo-item'
 function Todos(){
   const [input, setInput] = useState('')
+  const [searchInput, setSearchInput] = useState('')
   const [sortBy, setSortBy] = useState('all')
   const [todos, setTodos] = useState([
     {key: 1, todo: "to market", complete: false},
@@ -18,11 +19,38 @@ function Todos(){
     }].concat(todos))
     setInput('')
   }
+  const searchItems = e => {
+    e.preventDefault();
+    console.log('search da items')
+    console.log(searchInput)
+    //fuzzy search
+    //trim() the todo
+    //split the searchInput toLowerCase each
+    const searchTerms = searchInput.toLowerCase().split(' ')
+    const fuzzy = todos.filter( todo => {
+      searchTerms.forEach( term => {
+        
+      })
+      return todo.todo.trim().toLowerCase()
+      //see which todo was hit most by the search terms to be higher up the list?
+    })
+    console.log({fuzzy, searchTerms})
+    //setTodos()
+  }
+  //maybe this should be the onChange of the input
+  //in this case we need to make it a useEffect/memo hook
+  //how to we make sure we only run after stoped typing, 3 seconds or so i guess
+  const handleChange = e => {
+    //setSearchInput(e.target.value)    
+  }
 
+  const clearSearch = e => {
+    console.log('clear the search')
+    setSearchInput('')
+  }
   const deleteItem = (item) => {   
     setTodos( todos.filter((curItem) => curItem.key !== item.key) )
   }
-  
   const completeItem = (item) => {
     setTodos(
       todos.map( (todo) => {
@@ -40,11 +68,21 @@ function Todos(){
     }, [setSortBy]
   )
 
+  const filteredTodos = todos.filter( todo => {
+    return sortBy !== 'all' 
+    ? sortBy === 'complete' ? todo.complete === true : todo.complete === false
+    : todo
+  })
+
   return(
     <div>
       <form onSubmit={addItem}>
         <input type="text" placeholder="Add task" value={input} onChange={e => setInput(e.target.value)}/>
         <button type="submit">Add</button>
+      </form>
+      <form role="search" onSubmit={searchItems}>
+        <span className="clear-search" onClick={clearSearch}>✗</span>
+        <input type="search" name="q" placeholder="search for items" aria-label="Search through site content" value={searchInput} onChange={e => setSearchInput(e.target.value)}/>
       </form>
       <ul className="filter">
         <li><button name="active" type="button" className={sortBy === 'active' ? 'selected' : ''} onClick={filterBy}>Active</button></li>
@@ -54,13 +92,14 @@ function Todos(){
       <div>
         <ul className="todos">
           {
-            todos.filter( todo => {
-              return sortBy !== 'all' 
-              ? sortBy === 'complete' ? todo.complete === true : todo.complete === false
-              : todo
-            }).map( todo => {
-              return <TodoItem key={todo.key} item={todo} handleDelete={deleteItem} handleComplete={completeItem}/>
-            })
+            filteredTodos.length > 0 
+            ? (
+              filteredTodos.map( todo => {
+                return <TodoItem key={todo.key} item={todo} handleDelete={deleteItem} handleComplete={completeItem}/>
+              })
+            )
+            : <li><span className="no-items">No items available</span></li>
+ 
           }
         </ul>
       </div>
