e5e1f4b2d9fac84d37f7ffb799980f2a5a7fc352
Eddcakes <eddadamson@gmail.com>
2019-06-05 00:55:05 +0100

    add react-spring & react-use-gesture

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {useSpring, animated} from 'react-spring'
 
+/*
 function TodoItem({item, handleDelete, handleComplete}){
   return(
     <li>
@@ -9,6 +11,29 @@ function TodoItem({item, handleDelete, handleComplete}){
         <span>{item.complete ? "✓" : ""}</span>
       </div>
     </li>
+  )
+}
+*/
+function TodoItem({item, handleDelete, handleComplete}){
+  //item.complete is our state/toggle true
+  const { x } = useSpring({ from: { x: 0 }, x: item.complete ? 1 : 0, config: { duration: 1000 } })
+  return(
+    <animated.li 
+      style={{
+        opacity: x.interpolate({ output: [0.3, 1] }),
+        transform: x
+          .interpolate({
+            range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+            output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
+          })
+          .interpolate(x => `scale(${x})`)
+      }}>
+      <span className="bin" onClick={() => handleDelete(item)}>🗑</span>
+      <span className={item.complete ? "complete" : "active"}>{item.todo}</span>
+      <div className="box" onClick={() => handleComplete(item)}>
+        <span>{item.complete ? "✓" : ""}</span>
+      </div>
+    </animated.li>
   )
 }
 
