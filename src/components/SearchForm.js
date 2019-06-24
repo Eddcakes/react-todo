585c8eda0f3e3ef17d05082b79a8ba6e3a1c0a51
Eddcakes <eddadamson@gmail.com>
2019-06-25 00:06:13 +0100

    replaced clear button and aligned in search box

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function SearchForm({searchItems, searchInput, handleSearchInput, clearSearch}){
   return (
-    <form role="search" onSubmit={searchItems} className="smol-form">
-      <input className="input-bar" type="search" name="q" placeholder="search for items" aria-label="Search through site content" value={searchInput} onChange={handleSearchInput}/>
-      <button type="button" className="clear-search btn-style" onClick={clearSearch}>✗</button>
+    <form role="search" className="search-form" onSubmit={searchItems}>
+      <input type="search" name="q" placeholder="search for items" aria-label="Search through site content" value={searchInput} onChange={handleSearchInput}/>
+      <span className="clear-search" onClick={clearSearch}>✗</span>
     </form>
   )
 }
