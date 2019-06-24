25248275202a8edce80613c015a58c36545187c0
Eddcakes <eddadamson@gmail.com>
2019-06-24 23:22:51 +0100

    changed clear from span to button

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,7 @@ function SearchForm({searchItems, searchInput, handleSearchInput, clearSearch}){
   return (
     <form role="search" onSubmit={searchItems} className="smol-form">
       <input className="input-bar" type="search" name="q" placeholder="search for items" aria-label="Search through site content" value={searchInput} onChange={handleSearchInput}/>
-      <span className="clear-search btn-style" onClick={clearSearch}>✗</span>
+      <button type="button" className="clear-search btn-style" onClick={clearSearch}>✗</button>
     </form>
   )
 }
