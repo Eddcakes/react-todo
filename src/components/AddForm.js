717d7048001abad864999a9987ed785244552726
Eddcakes <eddadamson@gmail.com>
2019-06-24 23:14:41 +0100

    add classes for styling and reset btn styles

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function AddForm({addItem, addInput, handleAddInput}){
   return (
-    <form onSubmit={addItem}>
-      <input type="text" placeholder="Add task" value={addInput} onChange={handleAddInput}/>
-      <button type="submit">Add</button>
+    <form onSubmit={addItem} className="smol-form">
+      <input className="input-bar" type="text" placeholder="Add task" value={addInput} onChange={handleAddInput}/>
+      <button type="submit" className="btn-style">Add</button>
     </form>
   )
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function SearchForm({searchItems, searchInput, handleSearchInput, clearSearch}){
   return (
-    <form role="search" onSubmit={searchItems}>
-      <input type="search" name="q" placeholder="search for items" aria-label="Search through site content" value={searchInput} onChange={handleSearchInput}/>
-      <span className="clear-search" onClick={clearSearch}>✗</span>
+    <form role="search" onSubmit={searchItems} className="smol-form">
+      <input className="input-bar" type="search" name="q" placeholder="search for items" aria-label="Search through site content" value={searchInput} onChange={handleSearchInput}/>
+      <span className="clear-search btn-style" onClick={clearSearch}>✗</span>
     </form>
   )
 }
