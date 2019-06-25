1249ca2c3ca40fa049d81a82756b64663e2fb391
Eddcakes <eddadamson@gmail.com>
2019-06-25 23:48:12 +0100

    improving search styles

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import cross from './cross.svg'
 
 function SearchForm({searchItems, searchInput, handleSearchInput, clearSearch}){
   return (
     <form role="search" className="search-form" onSubmit={searchItems}>
-      <input type="search" name="q" placeholder="search for items" aria-label="Search through site content" value={searchInput} onChange={handleSearchInput}/>
-      <span className="clear-search" onClick={clearSearch}>✗</span>
+      <input type="search" name="q" autoComplete="off" placeholder="search for items" aria-label="Search through site content" value={searchInput} onChange={handleSearchInput}/>
+      <span className="clear-search" onClick={clearSearch}><img src={cross} alt="logo"/></span>
+      
     </form>
   )
 }
