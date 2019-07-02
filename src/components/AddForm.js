acea07e1e1b4eca0c80d5370fae79afd6990e5f5
Eddcakes <eddadamson@gmail.com>
2019-07-02 23:05:21 +0100

    styled add new task

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AddForm({addItem, addInput, handleAddInput}){
   return (
     <form onSubmit={addItem} className="add-form">
-      <input className="input-bar" type="text" placeholder="Add task" value={addInput} onChange={handleAddInput}/>
+      <input className="input-bar" type="text" placeholder="Add a new task" value={addInput} onChange={handleAddInput}/>
       <button type="submit" className="btn-style">Add</button>
     </form>
   )
